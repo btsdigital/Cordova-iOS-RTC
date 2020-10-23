1f8118508b9cc9b2457a3298e7419e7ff1fe737c
Almas Adilbek <almas.adilbek@gmail.com>
2020-10-23 12:49:11 +0600

    New init

---
+++ CordovaRTC.podspec
@@ -0,0 +1,17 @@
+Pod::Spec.new do |s|
+  s.name                  = 'CordovaRTC'
+  s.version               = '1.0.3'
+  s.summary               = 'CordovaRTC plugin'
+  s.description           = <<-DESC
+CordovaRTC cordova plugin for iOS
+                       DESC
+  s.homepage              = 'https://digital-id.kz'
+  s.license               = 'MIT'
+  s.author                = { 'DigitalID' => 'almas.adilbek@btsdigital.kz' }
+  s.source                = { :git => 'https://github.com/btsdigital/Cordova-iOS-RTC.git', :tag => "v#{s.version}" }
+  s.source_files          = 'CordovaRTC/*.{h,m,swift}'
+  s.dependency              'Cordova'
+  s.vendored_frameworks   = 'CordovaRTC/WebRTC.framework'
+  s.ios.deployment_target = '11.0'
+  s.swift_version         = '4.2'
+end
---
+++ CordovaRTC/CordovaRTC.h
@@ -0,0 +1,38 @@
+//
+//  CordovaRTC.h
+//  CordovaRTC
+//
+//  Created by BTSD on 10/21/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CordovaRTC.
+FOUNDATION_EXPORT double CordovaRTCVersionNumber;
+
+//! Project version string for CordovaRTC.
+FOUNDATION_EXPORT const unsigned char CordovaRTCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CordovaRTC/PublicHeader.h>
+
+#import <Cordova/CDVViewController.h>
+//#import "cordova-plugin-iosrtc-Bridging-Header.h"
+
+//#import <WebRTC/WebRTC.h>
+#import <WebRTC/RTCAudioTrack.h>
+#import <WebRTC/RTCDataChannel.h>
+#import <WebRTC/RTCDtmfSender.h>
+#import <WebRTC/RTCEAGLVideoView.h>
+#import <WebRTC/RTCIceCandidate.h>
+#import <WebRTC/RTCIceServer.h>
+#import <WebRTC/RTCMediaConstraints.h>
+#import <WebRTC/RTCMediaSource.h>
+#import <WebRTC/RTCMediaStream.h>
+#import <WebRTC/RTCMediaStreamTrack.h>
+#import <WebRTC/RTCPeerConnection.h>
+#import <WebRTC/RTCPeerConnectionFactory.h>
+#import <WebRTC/RTCSessionDescription.h>
+#import <WebRTC/RTCVideoCapturer.h>
+#import <WebRTC/RTCVideoRenderer.h>
+#import <WebRTC/RTCVideoSource.h>
+#import <WebRTC/RTCVideoTrack.h>
